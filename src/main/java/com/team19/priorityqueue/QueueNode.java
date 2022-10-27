593f2cefe2e469eb1ad72c7fbecbb2ae8ce1c7e6
pawl1n <pinchuk.andryusha310212@gmail.com>
2022-10-27 14:17:35 +0300

    feat: all buttons now work

--- src/main/java/com/team19/priorityqueue/QueueNode.java
+++ src/main/java/com/team19/priorityqueue/QueueNode.java
@@ -1,28 +1,24 @@
 package com.team19.priorityqueue;
 
-import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
-public class QueueNode<T>implements Comparable<QueueNode<T>> {
+public class QueueNode<T> {
     private final StackPane stackPane;
     private final T value;
     private final int priority;
     private final Circle circle;
 
     public QueueNode(T value, int priority) {
-        Circle circle = new Circle(20);
+        circle = new Circle(20);
         circle.setFill(Color.WHITE);
         circle.setStrokeWidth(5);
 
         Text text = new Text(value.toString());
-        StackPane stackPane = new StackPane(circle, text);
+        stackPane = new StackPane(circle, text);
 
-        this.circle = circle;
-        this.stackPane = stackPane;
         this.value = value;
         this.priority = priority;
     }
@@ -39,8 +35,7 @@ public class QueueNode<T>implements Comparable<QueueNode<T>> {
         return this.priority;
     }
 
-    @Override
-    public int compareTo(QueueNode queueNode) {
+    public int comparePriorityTo(QueueNode<T> queueNode) {
         return Integer.compare(priority, queueNode.priority());
     }
 
